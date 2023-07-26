1c2a8559fb2fe02a736f61db7c708b6b80f29bcd
Jiwon Kang <jiwonkang@skku.edu>
2023-07-26 23:17:38 +0900

    tip: itertools.product 대신에 dfs 사용
    테스트 1 〉	통과 (0.03ms, 10.4MB)
    테스트 2 〉	통과 (0.04ms, 10.4MB)
    테스트 3 〉	통과 (0.03ms, 10.2MB)
    테스트 4 〉	통과 (0.03ms, 10.2MB)
    테스트 5 〉	통과 (0.76ms, 10.5MB)
    테스트 6 〉	통과 (1.92ms, 10.2MB)
    테스트 7 〉	통과 (1.20ms, 10.2MB)
    테스트 8 〉	통과 (0.27ms, 10.3MB)
    테스트 9 〉	통과 (397.15ms, 10.3MB)
    테스트 10 〉	통과 (355.12ms, 10.4MB)
    테스트 11 〉	통과 (368.48ms, 10.6MB)
    테스트 12 〉	통과 (373.78ms, 10.4MB)
    테스트 13 〉	통과 (401.91ms, 10.4MB)
    테스트 14 〉	통과 (358.26ms, 10.3MB)
    테스트 15 〉	통과 (366.26ms, 10.4MB)
    테스트 16 〉	통과 (368.64ms, 10.4MB)
    테스트 17 〉	통과 (382.79ms, 10.4MB)
    테스트 18 〉	통과 (348.31ms, 10.5MB)
    테스트 19 〉	통과 (344.77ms, 10.5MB)
    테스트 20 〉	통과 (351.68ms, 10.5MB)

--- 구현/프로그래머스-214288-상담원 인원.py
+++ 구현/프로그래머스-214288-상담원 인원.py
@@ -1,25 +1,30 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/214288#
 
 def solution(k, n, reqs):
-    """
-    2126
-    2250 1차 시도 - 테스트 8 실패
-    """
     from collections import defaultdict
-    from itertools import product
     import heapq as hq
+    
+    cand = []
+    answer = 1e9
     
     k2reqs = defaultdict(list)
     for req in reqs:
         k2reqs[req[-1]].append(req)
         
-
-    answer = 1e9
     
-    for mentors in product(range(1, (n-k)+2), repeat = k):
-        if sum(mentors) != n:
-            continue
-        
+    def dfs(cumsum, path):
+        nonlocal cand, n, k
+        for i in range(1, (n-k)+2):
+            nextsum = cumsum + i
+            if nextsum == n and len(path) + 1 == k:
+                cand.append(path + [i])
+            elif nextsum < n and len(path) + 1 < k:
+                dfs(nextsum, path + [i])  
+                
+    dfs(0, [], n, k)
+    
+    
+    for mentors in cand:
         wait = 0
         for _type, num_mentors in enumerate(mentors):
             jobs = [0] * num_mentors # 각 멘토별 예상 작업 종료 시간 저장
